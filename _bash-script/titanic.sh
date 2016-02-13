23b1965620bab7670e85cc4f8c8ff3880b637d04
Mark Ormesher <me@markormesher.co.uk>
2016-02-13 15:24:50 +0000

    Bash script initialisation

--- _bash-script/titanic.sh
+++ _bash-script/titanic.sh
@@ -23,6 +23,7 @@ printHelp () {
 	out "Usage: titanic [action]"
 	out ""
 	out "Configuration:"
+	out "  --init                      Set up the initial files and this machine's identity"
 	out "  --set-config {key} {value}  Set a given configuration option"
 	out "  --show-config {key}         Show a specific value from the current local configuration"
 	out "  --show-config               Show the entire current local configuration"
@@ -41,17 +42,68 @@ printHelp () {
 
 configFolder="${HOME}/.titanic"
 configFile="${configFolder}/titanic.config"
+scriptsFile="${configFolder}/titanic-scripts"
 
 # check folder/file exists
 mkdir -p "${configFolder}"
 touch "${configFile}"
+touch "${scriptsFile}"
 
 # load default config
-configKeys=("machineIdentity")
+configKeys=("serverPath" "machineIdentity" "hookFile" "hostsFile")
+_serverPath="unknown"
 _machineIdentity="unknown"
+_hookFile="${HOME}/.bashrc"
+_hostsFile="/etc/hosts"
 
 # load config from file, overwriting some of the above
 . ${configFile}
+
+init () {
+	# check whether an init file exists
+	if [ -e "${_hookFile}" ] && grep -Fxq "# START TITANIC" "${_hookFile}";
+	then
+		out "WARNING: init has already been done"
+		exit 1
+	fi
+
+	# intro
+	out "Welcome to Titanic! ${version}"
+	out "Please answer the following questions to complete setup:"
+	out ""
+
+	# get the server path
+	read -p "  Titanic server path: " tmpInput
+	writeConfig "serverPath" "${tmpInput}"
+
+	# get the machine identity
+	read -p "  Machine identity: " tmpInput
+	writeConfig "machineIdentity" "${tmpInput}"
+
+	# get the hook file
+	read -p "  Hook file: (${_hookFile}) " tmpInput
+	if [ -z "${tmpInput}" ];
+	then
+		tmpInput="${_hookFile}"
+	fi
+	writeConfig "hookFile" "${tmpInput}"
+
+	# get the hosts file
+	read -p "  Hosts file: (${_hostsFile}) " tmpInput
+	if [ -z "${tmpInput}" ];
+	then
+		tmpInput="${_hostsFile}"
+	fi
+	writeConfig "hostsFile" "${tmpInput}"
+
+	# set up hook files
+	printf "\n\n# START TITANIC\n. ${scriptsFile}\n# END TITANIC\n" >> "${_hookFile}"
+	. "${scriptsFile}"
+
+	# done
+	out "Done!"
+	exit 0
+}
 
 # write a config item to the file
 writeConfig () { # 1: key; 2: value
@@ -69,8 +121,8 @@ writeConfig () { # 1: key; 2: value
 	cat "${configFile}.bak" | grep -v "^_${1}=" > "${configFile}"
 	echo "_${1}=${2}" >> "${configFile}"
 
-	# finish
-	exit 0
+	# reload the config
+	. "${configFile}"
 }
 
 # show all config details
@@ -78,7 +130,10 @@ printConfig () { # 1: key (optional)
 	if [ $# -eq 0 ]
 	then
 		# print entire config
+		out "serverPath       =  ${_serverPath}"
 		out "machineIdentity  =  ${_machineIdentity}"
+		out "hookFile         =  ${_hookFile}"
+		out "hostsFile        =  ${_hostsFile}"
 	else
 		# check the key given is valid
 		if ! elementIn "${1}" "${configKeys[@]}"
@@ -91,8 +146,6 @@ printConfig () { # 1: key (optional)
 		key="_${1}"
 		out "${!key}"
 	fi
-
-	exit 0
 }
 
 ##########
@@ -102,7 +155,6 @@ printConfig () { # 1: key (optional)
 _sync () {
 	# todo
 	out "Sync"
-	exit 0
 }
 
 #######################
@@ -123,11 +175,17 @@ while test $# -gt 0; do
 
 		# configuration
 
+		--init)
+			init
+			exit 0
+		;;
+
 		--set-config)
 			shift
 			if [ $# -gt 1 ]
 			then
 				writeConfig "${1}" "${2}"
+				exit 0
 			else
 				out "You must specify a key and a parameter"
 				exit 1
@@ -139,8 +197,10 @@ while test $# -gt 0; do
 			if [ $# -eq 1 ]
 			then
 				printConfig "${1}"
+				exit 0
 			else
 				printConfig
+				exit 0
 			fi
 			;;
 
@@ -148,12 +208,14 @@ while test $# -gt 0; do
 
 		-s|--sync)
 			_sync
+			exit 0
 			;;
 
 		# misc
 
 		-?|--help)
 			printHelp
+			exit 0
 			;;
 
 		*)
